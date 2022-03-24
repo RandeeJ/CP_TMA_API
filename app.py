a34d68595f3ecc402bbbc4bc28b442af601c2e4b
Randee Johnson <razzlepazzle@gmail.com>
2022-03-23 21:55:06 -0400

    cleaned up

--- app.py
+++ app.py
@@ -6,20 +6,12 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#will create a new instance of Flask and will call and store it in this variable. Later can call and run it.
 
-# @app.route("/")
-# #creating a route (endpoint)
-# def hello():
-#     return "Hey Flask"
 
-# Must have base directory so that flask knows where to save table
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 db = SQLAlchemy(app)
-# ^gives us a programmatic way of interacting with the database
 ma = Marshmallow(app)
-# ^allows us to have schema --> stucture to the database
 
 CORS(app)
 
@@ -29,8 +21,7 @@ class Calculation(db.Model):
     valueOne = db.Column(db.Integer, unique=False)
     valueTwo = db.Column(db.Integer, unique=False)
     valueAnswer = db.Column(db.Integer, unique=False)
-    operation = db.Column(db.String, unique=False) #multiplicatin sum 
-    # ^columns and data types
+    operation = db.Column(db.String, unique=False) 
 
     def __init__(self, valueOne, operation, valueTwo, valueAnswer):
         self.valueOne = valueOne
@@ -41,23 +32,17 @@ class Calculation(db.Model):
 class CalculationSchema(ma.Schema):
     class Meta:
         fields = ('id','valueOne', 'operation', 'valueTwo', 'valueAnswer')
-        # is there a way to add a column based on the operation button that was selected?
 
 calculation_schema = CalculationSchema()
-# ^a single guide schema
 calculations_schema = CalculationSchema(many=True)
-# ^a multiple guides schema
 
 
 
 
-#Endpoint to create a new calculation
 @app.route('/calculation', methods=["POST"])
-#^anytime you want to create something in the database
 def add_calculation():
     print(request.get_json())
     valueOne = request.json['valueOne']
-    # ^will get this object and then be able to parse it
     operation = request.json['operation']
     valueTwo = request.json['valueTwo']
     valueAnswer = request.json['valueAnswer']
@@ -65,7 +50,6 @@ def add_calculation():
     new_calculation = Calculation(valueOne, operation, valueTwo, valueAnswer)
 
     db.session.add(new_calculation)
-    # ^creates a new database session and adds a new guide inside of it
 
     db.session.commit()
 
@@ -77,23 +61,18 @@ def add_calculation():
 
 
 
-# Endpoint to query all calculations
 @app.route('/calculations', methods=["GET"])
 def get_calculations():
     all_calculations = Calculation.query.all()
-    # ^will bring back all of the guides in the system
     result = calculations_schema.dump(all_calculations)
     return jsonify(result)
-    # video says result.data (in v3.0 no need to call data, dump already calls data)
 
 
 
 
 
-# Endpoint for querying a single calculation
 @app.route("/calculation/<id>", methods=["GET"])
 def get_calculation(id):
-    #note that we pass in an argument here
     calculation = Calculation.query.get(id)
     return calculation_schema.jsonify(calculation)
 
@@ -102,7 +81,6 @@ def get_calculation(id):
 
 
 
-# Endpoint for updating a calculation
 @app.route("/calculation/<id>", methods=["PUT"])
 def calculation_update(id):
     calculation = Calculation.query.get(id)
@@ -124,7 +102,6 @@ def calculation_update(id):
 
 
 
-# Endpoint for deleting a calculation
 @app.route("/calculation/<id>", methods=["DELETE"])
 def calculation_delete(id):
     calculation = Calculation.query.get(id)
@@ -138,4 +115,3 @@ def calculation_delete(id):
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #
